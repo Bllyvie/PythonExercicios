b19b76bab6e848e4dc35df767bb58d2a3f66ff34
be_bebelly@outlook.com <be_bebelly@outlook.com>
2024-07-26 00:01:36 -0300

    python

--- modulo-01/python-exercicios/ex008.py
+++ modulo-01/python-exercicios/ex008.py
@@ -1,5 +1,9 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 M = float(input('Qual o valor a ser convertido? '))
-cm = 100 * M
-mm = 1000 * M
-print('O valor em centímetros é de {} \nE em milímetros é de {}'.format(cm, mm))
+cm = M * 100
+mm = M * 1000
+dm = M * 10
+dam = M / 10
+hm = M / 100
+km = M / 1000
+print('A medida de {:.0f}m corresponde a\n {:.0f}cm\n {:.0f}mm\n {:.0f}dm\n {}dam\n {}hm\n {}km\n'.format(M, cm, mm, dm, dam, hm, km))
