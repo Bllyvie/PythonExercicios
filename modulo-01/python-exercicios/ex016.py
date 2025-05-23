b259ddb75e0cf718060aef137dd95e9de3b78407
Bllyvie <be_bebelly@outlook.com>
2025-05-22 21:41:28 -0300

    python

--- modulo-01/python-exercicios/ex016.py
+++ modulo-01/python-exercicios/ex016.py
@@ -1,2 +1,9 @@
+'''from math import trunc
+num = float(input('Digite um número quebrado:'))
+inteiro = trunc(num)
+print(f'Este número {num} tem a parte inteira {inteiro}')'''
+
+num = float(input('Digite um valor quebrado: '))
+print(f'Este número {num} tem a parte inteira {int(num)}')
 #Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 #Ex: Digite um número: 6.127. O número 6.127 tem a parte Inteira 6.
--- modulo-01/python-exercicios/ex017.py
+++ modulo-01/python-exercicios/ex017.py
@@ -1,1 +1,5 @@
+a = int(input('Valor do cateto oposto: '))
+b = int(input('Valor do cateto adjacente: '))
+c = (a**2 + b**2) **(1/2)
+print(f'O comprimento da hipotenusa é: {c}')
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
